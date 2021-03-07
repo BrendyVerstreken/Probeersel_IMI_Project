6a543d92d3f4718dad105d4772f92430bf658aa2
Brendy Verstreken <brendy.verstreken@student.howest.be>
2021-03-07 20:01:46 +0100

    Add the entitities for the labraryPage

---
+++ src/Imi.Project.Blazor/Models/LabraryItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Imi.Project.Blazor.Models
+{
+    public class LabraryItem
+    {
+        public int Id { get; set; }
+        public string Tilte { get; set; }
+        public int GenreId { get; set; }
+        public int YearRelease { get; set; }
+        public string Director { get; set; }
+        public string ProductionHouse { get; set; }
+        public string Description { get; set; }
+        public InputSelectItem[] Genres { get; set; }
+    }
+}
